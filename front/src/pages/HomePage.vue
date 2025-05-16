d9dc6769ea29dd313131f1baba47d2fc72561753
Anthony LALBA <a.lalba@agoravita.com>
2025-05-16 12:01:43 +0200

    Suite inté

--- front/src/pages/HomePage.vue
+++ front/src/pages/HomePage.vue
@@ -1,9 +1,14 @@
 <template>
   <q-page class="flex flex-center">
-    WIP
+    <div class="q-pa-md row items-start q-gutter-md">
+      <QuoteItem />
+
+
+    </div>
   </q-page>
 </template>
 
 <script setup>
-//
+import QuoteItem from 'components/QuoteItemComponent.vue'
+
 </script>
