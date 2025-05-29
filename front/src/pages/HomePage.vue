cbbec3c81e3c0b7c2212ecdf978e213929bda2a1
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-05-29 15:09:42 +0200

    Some style on front

--- front/src/pages/HomePage.vue
+++ front/src/pages/HomePage.vue
@@ -1,9 +1,12 @@
 <template>
-  <q-page class="flex flex-center">
-    WIP
+  <q-page>
+    <q-toolbar inset style="padding: 0 !important;">
+      <Suggesteds />
+    </q-toolbar>
   </q-page>
 </template>
 
 <script setup>
-//
+import Suggesteds from 'components/SuggestedsComponent.vue'
+
 </script>
